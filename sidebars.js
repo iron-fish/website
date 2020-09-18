76ddae0cec473c5243c338da6df74c9108e83363
johann kerbrat <johannkerbrat@johanns-MacBook-Pro.local>
2020-09-18 11:48:21 -0700

    first version stable

---
+++ sidebars.js
@@ -0,0 +1,7 @@
+module.exports = {
+  someSidebar: {
+    'Whitepaper': ['whitepaper_1_introduction', 'whitepaper_2_data_structure_models', 'whitepaper_11_appendix'],
+    Docusaurus: ['doc1', 'doc2', 'doc3'],
+    Features: ['mdx'],
+  },
+};
