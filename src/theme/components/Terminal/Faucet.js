692c4f643a20b85dfbe934bc33a84012752d2aa6
Johann Kerbrat <johann@kerbrat.eu>
2021-03-01 15:26:49 -0800

    first list of changes

--- src/theme/components/Terminal/Faucet.js
+++ src/theme/components/Terminal/Faucet.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 export default [
-      <span data-ty="input">ironfish faucet:giveme</span>,
-      <span data-ty>
-        {`
+  <span data-ty="input">ironfish faucet:giveme</span>,
+  <span data-ty className="notVisibleOnMobile">
+    {`
 ::::::::::          :::::::::::::::::
 ::::::::::::       :::::::::::::::::::
 :::::::::::::     :::::::::::::::::::::
@@ -16,25 +16,31 @@ export default [
 :::::::::::::     :::::::::::::::::::::
 ::::::::::::       :::::::::::::::::::
 ::::::::::           ::::::::::::::::
-
+`}
+  </span>,
+  <span data-ty>
+    {`
 Receive funds, check your balance and send money.
 
 Thanks for contributing to Iron Fish!
 `}
-      </span>,
-      <span
-        data-ty="input"
-        data-ty-type-delay="400"
-        data-ty-prompt="Enter your email to stay updated with Iron Fish:"
-      >
-        newfish@ironfish.network
-      </span>,
-      <span data-ty="progress"></span>,
-      <span data-ty>
-        {`
+  </span>,
+  <span
+    data-ty="input"
+    data-ty-type-delay="400"
+    data-ty-prompt="Enter your email to stay updated with Iron Fish:"
+  >
+    newfish@ironfish.network
+  </span>,
+  <span data-ty="progress"></span>,
+  <span data-ty>
+    {`
 Added newfish@ironfish.network to our newsletter.
 Faucet request successfully added to the queue for key: 12a3e7fa7c09eedfdb1a4c4c87a7cf9a45bd8dab6982c82b59685d5c03486429a5f32faf4375aeb9e79e6b.
-
+`}
+  </span>,
+  <span data-ty className="notVisibleOnMobile">
+    {`
 ::::::::::          :::::::::::::::::             ::::::::::          :::::::::::::::::
 ::::::::::::       :::::::::::::::::::            ::::::::::::       :::::::::::::::::::
 :::::::::::::     :::::::::::::::::::::           :::::::::::::     :::::::::::::::::::::
@@ -47,7 +53,10 @@ Faucet request successfully added to the queue for key: 12a3e7fa7c09eedfdb1a4c4c
 :::::::::::::     :::::::::::::::::::::           :::::::::::::     :::::::::::::::::::::
 ::::::::::::       :::::::::::::::::::            ::::::::::::       :::::::::::::::::::
 ::::::::::           ::::::::::::::::             ::::::::::           ::::::::::::::::
-
+`}
+  </span>,
+  <span data-ty>
+    {`
 Congratulations! The Iron Fish Faucet just sent funds to your account!
 
 Check your balance by running:
@@ -56,5 +65,5 @@ Check your balance by running:
 Learn how to send a transaction by running:
 - ironfish accounts:pay --help
 `}
-      </span>
+  </span>,
 ];
