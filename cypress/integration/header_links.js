9765b326816fe2bd94c7c34be578dd51a5cca4b2
Yury Starav <yury@ironfish.network>
2021-09-14 17:17:13 -0700

    IRO-1031: Validate that all links on about us page opens successfully

--- cypress/integration/header_links.js
+++ cypress/integration/header_links.js
@@ -1,6 +1,6 @@
 describe('Headers Links', () => {
     it('Validate "Get Started" header link', () => {
-      cy.visit('https://ironfish.network/')
+      cy.visit('/')
       cy.get('a.navbar__item').contains('Get started').click()
       cy.url().should('include', 'docs/onboarding/iron-fish-tutorial')
     })
