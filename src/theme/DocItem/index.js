1cb904ec39a59c2f799aa0475e7c65013391e437
Elena Nadolinski <elena@ironfish.network>
2021-11-30 07:40:35 -0800

    Nov 30 launch branch (#114)
    * WIP: IRO-790 Add countdown banner to main website (#81)
    
    * IRO-790 Add countdown banner to main website
    
    * updating count down time
    
    * Adding Discord link for CTA
    
    Co-authored-by: Elena Nadolinski <elena@ironfish.network>
    
    * Series A + Incentivized testnet blog post (#113)
    
    * Removing "Edit this page" link on the bottom
    
    * Fixing banner layout on mobile
    
    * Try using image tag for blog image (#115)
    
    Co-authored-by: Derek Guenther <derek@ironfish.network>

--- src/theme/DocItem/index.js
+++ src/theme/DocItem/index.js
@@ -4,26 +4,26 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import React from 'react';
-import Head from '@docusaurus/Head';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import DocPaginator from '@theme/DocPaginator';
-import DocVersionSuggestions from '@theme/DocVersionSuggestions';
-import TOC from '@theme/TOC';
-import clsx from 'clsx';
-import styles from './styles.module.css';
+import React from "react";
+import Head from "@docusaurus/Head";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import DocPaginator from "@theme/DocPaginator";
+import DocVersionSuggestions from "@theme/DocVersionSuggestions";
+import TOC from "@theme/TOC";
+import clsx from "clsx";
+import styles from "./styles.module.css";
 import {
   useActivePlugin,
   useVersions,
   useActiveVersion,
-} from '@theme/hooks/useDocs';
+} from "@theme/hooks/useDocs";
 
 function DocItem(props) {
-  const {siteConfig = {}} = useDocusaurusContext();
-  const {url: siteUrl, title: siteTitle} = siteConfig;
-  const {content: DocContent} = props;
-  const {metadata} = DocContent;
+  const { siteConfig = {} } = useDocusaurusContext();
+  const { url: siteUrl, title: siteTitle } = siteConfig;
+  const { content: DocContent } = props;
+  const { metadata } = DocContent;
   const {
     description,
     title,
@@ -40,7 +40,7 @@ function DocItem(props) {
       hide_table_of_contents: hideTableOfContents,
     },
   } = DocContent;
-  const {pluginId} = useActivePlugin({
+  const { pluginId } = useActivePlugin({
     failfast: true,
   });
   const versions = useVersions(pluginId);
@@ -63,7 +63,7 @@ function DocItem(props) {
           <meta property="og:description" content={description} />
         )}
         {keywords && keywords.length && (
-          <meta name="keywords" content={keywords.join(',')} />
+          <meta name="keywords" content={keywords.join(",")} />
         )}
         {metaImage && <meta property="og:image" content={metaImageUrl} />}
         {metaImage && <meta property="twitter:image" content={metaImageUrl} />}
@@ -74,10 +74,10 @@ function DocItem(props) {
         {permalink && <link rel="canonical" href={siteUrl + permalink} />}
       </Head>
       <div
-        className={clsx('container padding-vert--lg', styles.docItemWrapper)}>
+        className={clsx("container padding-vert--lg", styles.docItemWrapper)}
+      >
         <div className="row">
-          <div
-            className={clsx('col', styles.docItemCol)}>
+          <div className={clsx("col", styles.docItemCol)}>
             <DocVersionSuggestions />
             <div className={styles.docItemContainer}>
               <article>
@@ -100,48 +100,25 @@ function DocItem(props) {
               {(editUrl || lastUpdatedAt || lastUpdatedBy) && (
                 <div className="margin-vert--xl">
                   <div className="row">
-                    <div className="col">
-                      {editUrl && (
-                        <a
-                          href={editUrl}
-                          target="_blank"
-                          rel="noreferrer noopener">
-                          <svg
-                            fill="currentColor"
-                            height="1.2em"
-                            width="1.2em"
-                            preserveAspectRatio="xMidYMid meet"
-                            viewBox="0 0 40 40"
-                            style={{
-                              marginRight: '0.3em',
-                              verticalAlign: 'sub',
-                            }}>
-                            <g>
-                              <path d="m34.5 11.7l-3 3.1-6.3-6.3 3.1-3q0.5-0.5 1.2-0.5t1.1 0.5l3.9 3.9q0.5 0.4 0.5 1.1t-0.5 1.2z m-29.5 17.1l18.4-18.5 6.3 6.3-18.4 18.4h-6.3v-6.2z" />
-                            </g>
-                          </svg>
-                          Edit this page
-                        </a>
-                      )}
-                    </div>
                     {(lastUpdatedAt || lastUpdatedBy) && (
                       <div className="col text--right">
                         <em>
                           <small>
-                            Last updated{' '}
+                            Last updated{" "}
                             {lastUpdatedAt && (
                               <>
-                                on{' '}
+                                on{" "}
                                 <time
                                   dateTime={new Date(
-                                    lastUpdatedAt * 1000,
+                                    lastUpdatedAt * 1000
                                   ).toISOString()}
-                                  className={styles.docLastUpdatedAt}>
+                                  className={styles.docLastUpdatedAt}
+                                >
                                   {new Date(
-                                    lastUpdatedAt * 1000,
+                                    lastUpdatedAt * 1000
                                   ).toLocaleDateString()}
                                 </time>
-                                {lastUpdatedBy && ' '}
+                                {lastUpdatedBy && " "}
                               </>
                             )}
                             {lastUpdatedBy && (
@@ -149,10 +126,10 @@ function DocItem(props) {
                                 by <strong>{lastUpdatedBy}</strong>
                               </>
                             )}
-                            {process.env.NODE_ENV === 'development' && (
+                            {process.env.NODE_ENV === "development" && (
                               <div>
                                 <small>
-                                  {' '}
+                                  {" "}
                                   (Simulated during dev for better perf)
                                 </small>
                               </div>
---
+++ src/theme/Navbar/TestnetBanner.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from "react";
+import {
+  differenceInHours,
+  differenceInMinutes,
+  formatDistanceStrict,
+} from "date-fns";
+
+// Increase by one minute so the clock stays at 1 minute left until
+// 1 sec after, rather than ticking to 0 at 59 secs before
+const LAUNCH_DATE = new Date("2021-12-01T16:01:00.000Z");
+
+const updateTimeRemaining = () => {
+  const now = new Date();
+
+  if (now > LAUNCH_DATE) {
+    return {
+      daysText: "0 days",
+      hoursText: "0 hours",
+      minutesText: "0 minutes",
+    };
+  }
+
+  const daysText = formatDistanceStrict(LAUNCH_DATE, now, {
+    unit: "day",
+    roundingMethod: "floor",
+  });
+
+  const hours = differenceInHours(LAUNCH_DATE, now) % 24;
+  const hoursText = `${hours} hour${hours !== 1 ? "s" : ""}`;
+
+  const minutes = differenceInMinutes(LAUNCH_DATE, now) % 60;
+  const minutesText = `${minutes} minute${minutes !== 1 ? "s" : ""}`;
+
+  return { daysText, hoursText, minutesText };
+};
+
+const TestnetBanner = () => {
+  const [time, setTime] = useState(updateTimeRemaining());
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setTime(updateTimeRemaining);
+    }, 10000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  });
+
+  return (
+    <div
+      style={{
+        background: "#e1ddff",
+        height: "auto",
+        fontSize: 18,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        textAlign: "center",
+        padding: "0.7rem",
+      }}
+    >
+      <span>
+        The Iron Fish Incentivized Testnet starts in {time.daysText},{" "}
+        {time.hoursText}, and {time.minutesText}.&nbsp;
+        <a
+          style={{ color: "#5141C9", textDecoration: "none" }}
+          href="https://discord.gg/EkQkEcm8DH"
+        >
+          Join Discord for updates
+        </a>
+      </span>
+    </div>
+  );
+};
+
+export default TestnetBanner;
