4bea28e634dfc9f295c61a9dd9a74c51fa5d23df
brekk <brekk@brekkbockrath.com>
2021-11-30 13:10:53 -0800

    update fei

--- src/theme/RoadmapPage/icon-fei.js
+++ src/theme/RoadmapPage/icon-fei.js
@@ -1,11 +1,26 @@
 import React from "react"
 export const Icon = () => (
-  <svg width="32" height="32" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path fill="#fff" d="M0 0h32v32H0z" />
-    <circle cx="16" cy="16" r="16" fill="#22996E" />
+  <svg
+    version="1.1"
+    id="Layer_1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    x="0px"
+    y="0px"
+    viewBox="0 0 18 18"
+    xmlSpace="preserve"
+  >
+    <circle class="st0" cx="9" cy="9" r="9" fill="#22996e" />
     <path
-      d="M16 23.128H7.244c-1.186 0-1.903-1.31-1.263-2.31l4.982-7.77a2 2 0 0 1 1.683-.92h6.708a2 2 0 0 1 1.683.92l4.982 7.77c.64 1-.077 2.31-1.263 2.31H16ZM16.895 5.255l2.092 3.197A1 1 0 0 1 18.151 10h-4.263a1 1 0 0 1-.828-1.562l2.17-3.197a1 1 0 0 1 1.665.014Z"
-      fill="#fff"
+      class="st1"
+      d="M9,13H4.1c-0.7,0-1.1-0.7-0.7-1.3l2.8-4.4C6.4,7,6.7,6.8,7.1,6.8h3.8c0.4,0,0.7,0.2,0.9,0.5l2.8,4.4
+	c0.4,0.6,0,1.3-0.7,1.3H9z"
+      fill="#ffffff"
+    />
+    <path
+      class="st1"
+      d="M9.5,3l1.2,1.8c0.2,0.4,0,0.9-0.5,0.9H7.8c-0.5,0-0.7-0.5-0.5-0.9l1.2-1.8C8.8,2.6,9.3,2.6,9.5,3z"
+      fill="#ffffff"
     />
   </svg>
 )
