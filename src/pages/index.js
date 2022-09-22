d5d6bc01d0d20249abb351e02be592c89de73b5e
Lawrence Wisne <lawrence@ironfish.network>
2022-09-22 13:39:11 -0400

    remove roadmap link while we figure out phase 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,21 +23,22 @@ const features = [
     buttonLink: "docs/whitepaper/1_introduction",
     button: "Read Our Whitepaper",
   },
-  {
-    id: "privacy-layer",
-    className: "sectionPrivacyLayer",
-    title: "A platform for the future of the internet",
-    description: (
-      <>
-        We’re building a new Layer 1 blockchain to become the universal privacy
-        layer for all of web3. Using zero-knowledge proofs (zk-SNARKs) and the
-        highest industry standards for encryption, we enable users to have fully
-        private transactions &mdash; a true SSL layer for all blockchains.
-      </>
-    ),
-    buttonLink: "/roadmap",
-    button: "See Our Roadmap",
-  },
+  // LW - 9/22 pulling this down while we figure out the Phase 3 plan
+  // {
+  //   id: "privacy-layer",
+  //   className: "sectionPrivacyLayer",
+  //   title: "A platform for the future of the internet",
+  //   description: (
+  //     <>
+  //       We’re building a new Layer 1 blockchain to become the universal privacy
+  //       layer for all of web3. Using zero-knowledge proofs (zk-SNARKs) and the
+  //       highest industry standards for encryption, we enable users to have fully
+  //       private transactions &mdash; a true SSL layer for all blockchains.
+  //     </>
+  //   ),
+  //   buttonLink: "/roadmap",
+  //   button: "See Our Roadmap",
+  // },
   {
     id: "privacy",
     className: "sectionCryptocurrency",
