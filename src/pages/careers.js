7dc68b76e46152dcf49359b7eecb184359f9f09d
ygao76 <yajun@ironfish.network>
2022-09-29 11:29:53 -0700

    Try a different color

--- src/pages/careers.js
+++ src/pages/careers.js
@@ -127,7 +127,7 @@ const employees = [
     title: "Ops",
     description:
       "Xoogler turned startup vet. Web3 hobbyist turned enthusiast. Orlando Magic fan turned prisoner.",
-    r: "#333333",
+    r: "#ffffff",
     l: "#0077C0",
     t: "#ffffff",
   },
