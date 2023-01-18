a099709da09d1122cb7fcc66e6ec37823e0f4d91
Jason Spafford <nullprogrammer@gmail.com>
2023-01-18 16:08:01 -0500

    Add wallet:balances to the website docs

---
+++ src/theme/components/Terminal/Wallet/CheckBalances.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+
+export default [
+    <span data-ty="input">ironfish wallet:balances</span>,
+    <span data-ty>
+    {`
+Account: default
+Asset Name Asset Id                                                         Confirmed Balance
+────────── ──────────────────────────────────────────────────────────────── ─────────────────
+$IRON      d7c86706f5817aa718cd1cfad03233bcd64a7789fd9422d3b17af6823a7e6ac6 24900000.00045900
+        `}
+  </span>,
+];
