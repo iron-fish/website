f7d3a7afddb7153e63429eb74932a2788daa1b8a
Derek Guenther <dguenther9@gmail.com>
2023-03-02 15:14:00 -0800

    Add offline signing to transactions page

---
+++ src/theme/components/Terminal/Wallet/Transaction.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+
+export default [
+    <span data-ty="input">ironfish wallet:transaction 24a7c779f53b9780c336d2388581be30360d2b6870e39fc39caa3cd2604b8d85</span>,
+    <span data-ty>
+    {`Transaction: 24a7c779f53b9780c336d2388581be30360d2b6870e39fc39caa3cd2604b8d85
+Account: default
+Status: confirmed
+Type: miner
+Timestamp: 3/1/2023 4:54:01 PM PST
+Fee: $IRON -20.00000000
+Block Hash: 000008ffef2da1a9b5881425721d1658f2978afe221b1d6b94d41818e476cef2
+Block Sequence: 54
+Notes Count: 1
+Spends Count: 0
+Mints Count: 0
+Burns Count: 0
+Sender: 5fd1137421b092339ce9d84be61593c75587b1e481cc25f46312bd97f56a8755
+---Notes---
+
+ Amount      Asset Name Asset Id Spent Memo Owner Owner Address                                         
+ ─────────── ────────── ──────── ───── ──── ───── ───────────────────────────────────────────────────── 
+ 20.00000000 $IRON      d7c8670… x          ✔     5fd1137421b092339ce9d84be61593c75587b1e481cc25f46312… 
+---Asset Balance Deltas---
+
+ Asset ID                                                         Balance Change 
+ ──────────────────────────────────────────────────────────────── ────────────── 
+ d7c86706f5817aa718cd1cfad03233bcd64a7789fd9422d3b17af6823a7e6ac6 2000000000`}
+  </span>,
+];
