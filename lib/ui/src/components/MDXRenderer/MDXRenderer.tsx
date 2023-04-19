399b9bc20af5686fe3c07192eeade3db43e8f8cd
Daniel Cortes <danielgcortes@gmail.com>
2023-04-19 15:48:13 -0600

    Style tweaks

--- lib/ui/src/components/MDXRenderer/MDXRenderer.tsx
+++ lib/ui/src/components/MDXRenderer/MDXRenderer.tsx
@@ -39,7 +39,7 @@ function HeadingWithAnchor(props: ComponentProps<typeof Heading>) {
         },
       }}
     >
-      {props.children}{" "}
+      {props.children}&nbsp;
       {headingId && (
         <Link href={`#${headingId}`} color="transparent">
           #
@@ -58,16 +58,28 @@ const rendererComponents: ComponentProps<typeof MDXRemote>["components"] = {
   h1: (props) => (
     <HeadingWithAnchor {...props} fontSize="2xl" my="8" fontWeight="medium" />
   ),
-  h2: (props) => <HeadingWithAnchor {...props} fontSize="3xl" my="6" />,
-  h3: (props) => <HeadingWithAnchor {...props} fontSize="2xl" my="6" />,
+  h2: (props) => <HeadingWithAnchor {...props} fontSize="3xl" mt="10" mb="6" />,
+  h3: (props) => <HeadingWithAnchor {...props} fontSize="2xl" mt="10" mb="6" />,
   h4: (props) => <HeadingWithAnchor {...props} fontSize="xl" my="4" />,
   h5: (props) => <HeadingWithAnchor {...props} fontSize="lg" my="4" />,
   p: (props) => <Text {...props} mb="6" {...DEFAULT_TEXT_PROPS} />,
   ul: (props) => (
-    <UnorderedList {...props} as="ul" spacing="2" {...DEFAULT_TEXT_PROPS} />
+    <UnorderedList
+      {...props}
+      as="ul"
+      spacing="2"
+      mb="6"
+      {...DEFAULT_TEXT_PROPS}
+    />
   ),
   ol: (props) => (
-    <OrderedList {...props} as="ol" spacing="2" {...DEFAULT_TEXT_PROPS} />
+    <OrderedList
+      {...props}
+      as="ol"
+      spacing="2"
+      mb="6"
+      {...DEFAULT_TEXT_PROPS}
+    />
   ),
   li: (props) => <ListItem {...props} as="li" {...DEFAULT_TEXT_PROPS} />,
   a: ({ href, children }) => {
