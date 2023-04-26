d938c9ab9f05d0ff25770efd0a8ef0b9b6fd8bbc
Daniel Cortes <danielgcortes@gmail.com>
2023-04-26 10:02:07 -0500

    Update copy

--- pages/company/about-us/index.tsx
+++ pages/company/about-us/index.tsx
@@ -6,12 +6,12 @@ import {
   Text,
   LocalImage,
   ThickLink,
-} from '@/lib/ui';
-import flower from '../../../assets/heroImages/about-us/flower.svg';
-import submarine from '../../../assets/heroImages/about-us/submarine.svg';
-import judge from '../../../assets/heroImages/about-us/judge.svg';
-import { FancyLinkSection } from '../../../components/FancyLinkSection/FancyLinkSection';
-import { Backers } from '../../../components/Company/Backers/Backers';
+} from "@/lib/ui";
+import flower from "../../../assets/heroImages/about-us/flower.svg";
+import submarine from "../../../assets/heroImages/about-us/submarine.svg";
+import judge from "../../../assets/heroImages/about-us/judge.svg";
+import { FancyLinkSection } from "../../../components/FancyLinkSection/FancyLinkSection";
+import { Backers } from "../../../components/Company/Backers/Backers";
 
 const flowerImage = flower as LocalImage;
 const submarineImage = submarine as LocalImage;
@@ -30,37 +30,37 @@ export default function AboutUs() {
             <HeroImageUtil
               image={flowerImage}
               top={{
-                md: '-150px',
-                xl: '-30px',
+                md: "-150px",
+                xl: "-30px",
               }}
               left={{
-                md: '-120px',
-                xl: '30px',
-                '2xl': `calc(50vw - 700px)`,
+                md: "-120px",
+                xl: "30px",
+                "2xl": `calc(50vw - 700px)`,
               }}
             />
             <HeroImageUtil
               image={submarineImage}
               bottom={{
-                md: '-80px',
-                xl: '15px',
+                md: "-80px",
+                xl: "15px",
               }}
               left={{
-                md: '-50px',
-                xl: '-20px',
-                '2xl': `calc(50vw - 850px)`,
+                md: "-50px",
+                xl: "-20px",
+                "2xl": `calc(50vw - 850px)`,
               }}
             />
             <HeroImageUtil
               image={judgeImage}
               top={{
-                md: '20px',
-                xl: '85px',
+                md: "20px",
+                xl: "85px",
               }}
               right={{
-                md: '-120px',
-                xl: '-20px',
-                '2xl': `calc(50vw - 700px)`,
+                md: "-120px",
+                xl: "-20px",
+                "2xl": `calc(50vw - 700px)`,
               }}
             />
           </>
@@ -70,13 +70,13 @@ export default function AboutUs() {
         <Text
           textStyle="h5"
           my={{
-            base: '50px',
-            md: '100px',
-            lg: '150px',
+            base: "50px",
+            md: "100px",
+            lg: "150px",
           }}
           textAlign="center"
         >
-          Iron Fish is building a decentralized{' '}
+          Iron Fish is building a decentralized{" "}
           <ThickLink underlineColor="pink.400">privacy solution</ThickLink> for
           crypto. Our Principles:
         </Text>
@@ -84,27 +84,27 @@ export default function AboutUs() {
       <FancyLinkSection
         heading="Encryption is the future of crypto"
         description={[
-          'Privacy is a fundamental right, and yet everyday people give away personal information — often without even realizing it.',
-          'We enable users to protect their privacy by encrypting all transactions with zero-knowledge proofs. We adopt the highest standards for protection without compromise.',
+          "Privacy is a fundamental right, and yet everyday people give away personal information — often without even realizing it.",
+          "We enable users to protect their privacy by encrypting all transactions with zero-knowledge proofs. We adopt the highest standards for protection without compromise.",
         ]}
-        ctaText="Learn more in our Whitepaper"
+        ctaText="Learn more"
         ctaLink="/learn/whitepaper"
         imageUrl="/images/about-us/encryption-fish.svg"
         containerProps={{
           maxW: {
-            base: '704px',
-            lg: '1600px',
+            base: "704px",
+            lg: "1600px",
           },
-          w: '100%',
+          w: "100%",
           mb: {
             base: 24,
             md: 32,
-            xl: '150px',
+            xl: "150px",
           },
           pl: {
-            lg: '40px',
-            xl: '64px',
-            '2xl': '128px',
+            lg: "40px",
+            xl: "64px",
+            "2xl": "128px",
           },
         }}
       />
@@ -118,16 +118,16 @@ export default function AboutUs() {
           imageUrl="/images/about-us/movement-fish.svg"
           ctaColor="green.400"
           containerProps={{
-            color: 'white',
+            color: "white",
             maxW: {
-              base: '704px',
-              lg: '1600px',
+              base: "704px",
+              lg: "1600px",
             },
-            w: '100%',
+            w: "100%",
             pr: {
-              lg: '40px',
-              xl: '64px',
-              '2xl': '128px',
+              lg: "40px",
+              xl: "64px",
+              "2xl": "128px",
             },
           }}
         />
@@ -141,14 +141,14 @@ export default function AboutUs() {
           imageUrl="/images/about-us/common-sense-fish.svg"
           containerProps={{
             maxW: {
-              base: '704px',
-              lg: '1600px',
+              base: "704px",
+              lg: "1600px",
             },
-            w: '100%',
+            w: "100%",
             pl: {
-              lg: '40px',
-              xl: '64px',
-              '2xl': '128px',
+              lg: "40px",
+              xl: "64px",
+              "2xl": "128px",
             },
           }}
         />
