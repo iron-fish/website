78fc03e06ac40ba88996841619579609a70ab485
Daniel Cortes <danielgcortes@gmail.com>
2023-05-03 12:22:06 -0600

    Undo Node App custom title

--- pages/use/node-app/index.tsx
+++ pages/use/node-app/index.tsx
@@ -5,7 +5,6 @@ import octopus from "../../../assets/heroImages/node-app/octopus.svg";
 import nodeApp from "../../../assets/heroImages/node-app/node-app.svg";
 import { NodeAppUIImage } from "../../../components/NodeAppUIImage/NodeAppImage";
 import { useIsClient } from "usehooks-ts";
-import Head from "next/head";
 
 const eelImage = eel as LocalImage;
 const octopusImage = octopus as LocalImage;
@@ -14,70 +13,65 @@ const appImage = nodeApp as LocalImage;
 export default function NodeApp() {
   const isClient = useIsClient();
   return (
-    <>
-      <Head>
-        <title>Node App • Iron Fish</title>
-      </Head>
-      <Box bg="orange.500">
-        <Hero
-          bg="orange.500"
-          heading="Node App"
-          subheading="Built for everyone"
-          description="Our desktop node app is launching soon! Sign up for our newsletter to be informed for its launch."
-          borderBottom="none"
-          textContainerProps={{
-            pb: {
-              base: "48px",
-              md: "48px",
-            },
-          }}
-          images={
-            <>
-              <HeroImageUtil
-                image={eelImage}
-                top={{
-                  md: "0px",
-                  xl: "20px",
-                }}
-                left={{
-                  md: "-120px",
-                  xl: "30px",
-                  "2xl": `calc(50vw - 700px)`,
-                }}
-              />
-              <HeroImageUtil
-                image={appImage}
-                bottom={{
-                  md: "20px",
-                  xl: "15px",
-                }}
-                left={{
-                  md: "-50px",
-                  xl: "-20px",
-                  "2xl": `calc(50vw - 850px)`,
-                }}
-              />
-              <HeroImageUtil
-                image={octopusImage}
-                top={{
-                  md: "20px",
-                  xl: "85px",
-                }}
-                right={{
-                  md: "-120px",
-                  xl: "-20px",
-                  "2xl": `calc(50vw - 700px)`,
-                }}
-              />
-            </>
-          }
-        >
-          <Flex mb="64px" justifyContent="center">
-            <NewsletterSignUp bordered />
-          </Flex>
-        </Hero>
-        <Box px={8}>{isClient && <NodeAppUIImage />}</Box>
-      </Box>
-    </>
+    <Box bg="orange.500">
+      <Hero
+        bg="orange.500"
+        heading="Node App"
+        subheading="Built for everyone"
+        description="Our desktop node app is launching soon! Sign up for our newsletter to be informed for its launch."
+        borderBottom="none"
+        textContainerProps={{
+          pb: {
+            base: "48px",
+            md: "48px",
+          },
+        }}
+        images={
+          <>
+            <HeroImageUtil
+              image={eelImage}
+              top={{
+                md: "0px",
+                xl: "20px",
+              }}
+              left={{
+                md: "-120px",
+                xl: "30px",
+                "2xl": `calc(50vw - 700px)`,
+              }}
+            />
+            <HeroImageUtil
+              image={appImage}
+              bottom={{
+                md: "20px",
+                xl: "15px",
+              }}
+              left={{
+                md: "-50px",
+                xl: "-20px",
+                "2xl": `calc(50vw - 850px)`,
+              }}
+            />
+            <HeroImageUtil
+              image={octopusImage}
+              top={{
+                md: "20px",
+                xl: "85px",
+              }}
+              right={{
+                md: "-120px",
+                xl: "-20px",
+                "2xl": `calc(50vw - 700px)`,
+              }}
+            />
+          </>
+        }
+      >
+        <Flex mb="64px" justifyContent="center">
+          <NewsletterSignUp bordered />
+        </Flex>
+      </Hero>
+      <Box px={8}>{isClient && <NodeAppUIImage />}</Box>
+    </Box>
   );
 }
