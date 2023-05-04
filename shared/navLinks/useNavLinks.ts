f2fe73d1574f00022b237225cd4b9e2409c86b6c
Rohan Jadvani <5459049+rohanjadvani@users.noreply.github.com>
2023-05-04 15:28:23 -0400

    feat(website): Remove link to testnet (#439)
    * feat(website): Remove link to testnet
    
    * fix(website): Remove href check

--- shared/navLinks/useNavLinks.ts
+++ shared/navLinks/useNavLinks.ts
@@ -7,13 +7,6 @@ export function useNavLinks(): NavItems {
   const { formatMessage } = useIntl();
   return useMemo(() => {
     return links.map((link) => {
-      if ("href" in link) {
-        return {
-          label: formatMessage(link.label),
-          href: link.href,
-        };
-      }
-
       const { label, items, color } = link;
       return {
         label: formatMessage(label),
