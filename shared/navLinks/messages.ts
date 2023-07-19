86beb2e5761499ad831b36e458efa61185a43486
Daniel Cortes <danielgcortes@gmail.com>
2023-07-19 10:15:29 -0500

    HackerOne link

--- shared/navLinks/messages.ts
+++ shared/navLinks/messages.ts
@@ -192,4 +192,12 @@ export const messages = defineMessages({
     defaultMessage:
       "Apply for funding with projects that improve our ecosystem.",
   },
+  bugBountyTitle: {
+    id: "nav.bugBountyTitle",
+    defaultMessage: "Bug Bounty",
+  },
+  bugBountyDescription: {
+    id: "nav.bugBountyDescription",
+    defaultMessage: "Participate in our cash incentivized bug bounty.",
+  },
 });
