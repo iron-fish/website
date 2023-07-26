f50465d4c08e460c62bb90b1e2f39fd5b7753615
Daniel Cortes <danielgcortes@gmail.com>
2023-07-26 10:40:28 -0500

    fix: Set up 301 redirects for broken links

--- next.config.js
+++ next.config.js
@@ -34,9 +34,104 @@ const nextConfig = {
       },
       {
         source: "/developers/documentation",
-        destination:
-          "/developers/documentation/integration_local",
+        destination: "/developers/documentation/integration_local",
         permanent: false,
+      },
+      {
+        source: "/use/wallet",
+        destination: "/use/node-app",
+        permanent: true,
+      },
+      {
+        source: "/docs/onboarding/iron-fish-tutorial",
+        destination: "/use/get-started/installation",
+        permanent: true,
+      },
+      {
+        source: "/docs/whitepaper/1_introduction",
+        destination: "/learn/whitepaper/introduction",
+        permanent: true,
+      },
+      {
+        source: "/docs/onboarding/installation-iron-fish",
+        destination: "/use/get-started/installation",
+        permanent: true,
+      },
+      {
+        source: "/docs/onboarding/miner-iron-fish",
+        destination: "/use/get-started/mining",
+        permanent: true,
+      },
+      {
+        source: "/tokenomics",
+        destination: "/learn/tokenomics",
+        permanent: true,
+      },
+      {
+        source: "/docs/onboarding/iron-fish-wallet-commands",
+        destination: "/use/get-started/cli-commands",
+        permanent: true,
+      },
+      {
+        source: "/docs/onboarding/start-an-iron-fish-node",
+        destination: "/use/get-started/running-a-node",
+        permanent: true,
+      },
+      {
+        source: "/docs/whitepaper/4_mining",
+        destination: "/learn/whitepaper/mining",
+        permanent: true,
+      },
+      {
+        source: "/docs/onboarding/iron-fish-cli",
+        destination: "/use/get-started/cli-commands",
+        permanent: true,
+      },
+      {
+        source: "/docs/whitepaper/5_account",
+        destination: "/learn/whitepaper/account",
+        permanent: true,
+      },
+      {
+        source: "/docs/onboarding/send-receive-iron-fish-transactions",
+        destination: "/use/get-started/transactions",
+        permanent: true,
+      },
+      {
+        source: "/docs/onboarding/iron-fish-node-health",
+        destination: "/use/get-started/node-health",
+        permanent: true,
+      },
+      {
+        source: "/docs/whitepaper/3_storage",
+        destination: "/learn/whitepaper/storage",
+        permanent: true,
+      },
+      {
+        source: "/docs/whitepaper/7_consensus_verification",
+        destination: "/learn/whitepaper/consensus-verification",
+        permanent: true,
+      },
+      {
+        source: "/docs/whitepaper/2_networking",
+        destination: "/learn/whitepaper/networking",
+        permanent: true,
+      },
+      {
+        source: "/docs/whitepaper/6_transaction",
+        destination: "/learn/whitepaper/transaction",
+        permanent: true,
+      },
+      {
+        source: "/docs/onboarding/rpc/chain",
+        destination:
+          "/developers/documentation/rpc/chain/broadcast_transaction",
+        permanent: true,
+      },
+      {
+        source: "/docs/onboarding/rpc/node",
+        destination: "/developers/documentation/rpc/node/get_log_stream",
+        permanent: true,
       },
     ];
   },
