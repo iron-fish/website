415296efe7de1b6119c6cf7d6950d60e41d5187b
Elena Nadolinski <elena@ironfish.network>
2023-08-09 16:31:17 -0700

    updating governance link

--- shared/navLinks/links.ts
+++ shared/navLinks/links.ts
@@ -116,7 +116,7 @@ export const links = [
       {
         title: messages.governanceTitle,
         description: messages.governanceDescription,
-        href: "https://discourse.ironfish.network/",
+        href: "https://discourse.ironfish.network/t/request-for-proposal-iron-fish-foundation-governance/23#interim-governance-structure-3",
         image: "/images/nav/community-governance.svg",
       },
       {
