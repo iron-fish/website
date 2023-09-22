6e0d084857249e66f5d434cd754f4ad3989ddb2d
Daniel Cortes <danielgcortes@gmail.com>
2023-09-22 16:50:20 -0500

    Media items dates

--- content/media/media.ts
+++ content/media/media.ts
@@ -21,13 +21,13 @@ export const MEDIA_ITEMS: Array<MediaItem> = [
   {
     title: `CEO Elena on Finnovators Podcast`,
     link: `https://open.spotify.com/episode/1ldMbhz3mT7CyePNiHbEUH?si=ASHL4gFQToeWtRgqh18z9A&nd=1&utm_content=buffer1f912`,
-    date: `2023-03-01`,
+    date: `2023-03-28`,
     image: "/images/media-items/finnovators.png",
   },
   {
     title: `"Do Crackdowns on Crypto Mixers Pose a Threat to Privacy?" CEO Elena adds perspective`,
     link: `https://beincrypto.com/can-crypto-mixer-crackdowns-harm-our-privacy/`,
-    date: `2023-03-01`,
+    date: `2023-03-23`,
     image: "/images/media-items/be-in-crypto.png",
   },
   {
@@ -39,43 +39,43 @@ export const MEDIA_ITEMS: Array<MediaItem> = [
   {
     title: `"Elena Nadolinski: Privacy for the People," feature produced by Sequoia Capital`,
     link: `https://www.sequoiacap.com/article/elena-nadolinski-spotlight/`,
-    date: `2022-10-01`,
+    date: `2022-10-07`,
     image: "/images/media-items/elena-sequoia.png",
   },
   {
     title: `CEO Elena at Messari Mainnet`,
     link: `https://www.youtube.com/watch?v=kru4oLjPD-A`,
-    date: `2022-09-01`,
+    date: `2022-10-04`,
     image: "/images/media-items/messari.png",
   },
   {
     title: `CEO Elena and General Counsel Craig on Around the Coin`,
     link: `https://www.youtube.com/watch?v=Vguia3WAI6Y`,
-    date: `2022-09-01`,
+    date: `2022-09-19`,
     image: "/images/media-items/around-the-coin.png",
   },
   {
     title: `CEO Elena and General Counsel Craig on The Encrypted Economy Podcast`,
     link: `https://www.youtube.com/watch?v=x-3Cct7_SP0`,
-    date: `2022-08-01`,
+    date: `2022-08-23`,
     image: "/images/media-items/encrypted-economy.png",
   },
   {
     title: `CEO Elena joins fellow crypto-industry privacy experts to discuss the fallout of Tornado Cash sanctions.`,
     link: `https://www.youtube.com/watch?v=s4RMbrMvC60`,
-    date: `2022-08-01`,
+    date: `2022-08-25`,
     image: "/images/media-items/empire.png",
   },
   {
     title: `“Why everyone should know about ZKPs,” CEO Elena on EthGlobal.TV`,
     link: `https://www.youtube.com/watch?v=61DuJAQXxjU`,
-    date: `2022-05-01`,
+    date: `2022-05-07`,
     image: "/images/media-items/eth-global.png",
   },
   {
     title: `"Privacy in Crypto is a 'Privilege Worth Keeping,'" Decrypt.co`,
     link: `https://decrypt.co/70129/privacy-privilege-worth-crypto-iron-fish`,
-    date: `2022-05-01`,
+    date: `2022-05-07`,
     image: "/images/media-items/decrypt.png",
   },
 ];
