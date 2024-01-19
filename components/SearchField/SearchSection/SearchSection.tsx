a1ac8949c1c97c18441dcef8d374c7ec339162c5
Daniel Cortes <danielgcortes@gmail.com>
2024-01-19 11:00:09 -0700

    Focusable search items

--- components/SearchField/SearchSection/SearchSection.tsx
+++ components/SearchField/SearchSection/SearchSection.tsx
@@ -28,18 +28,26 @@ function Item({
   href?: string;
   onClick?: () => void;
 }) {
+  const marginOffset = "12px";
   return (
     <HStack
       gap={3}
-      mx={-3}
-      px={3}
+      mx={`-${marginOffset}`}
+      px={marginOffset}
       py={2}
       borderRadius={4}
       onClick={onClick}
       cursor={onClick ? "pointer" : undefined}
+      _focus={{
+        backgroundColor: "#F6F6F6",
+        outline: 0,
+      }}
       _hover={{
         backgroundColor: "#F6F6F6",
       }}
+      as={onClick ? "button" : "div"}
+      textAlign="left"
+      w={`calc(100% + ${marginOffset} * 2)`}
     >
       <Box fontSize="1.4rem">{icon}</Box>
       <Box minW={0}>{children}</Box>
