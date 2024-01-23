1e41c33a3065c273fd02c67a9ec540edb33a3361
Daniel Cortes <danielgcortes@gmail.com>
2024-01-23 13:38:41 -0700

    Make featured blog its own component

--- pages/learn/blog/index.tsx
+++ pages/learn/blog/index.tsx
@@ -1,42 +1,22 @@
 import { readdirSync } from "fs";
 import path from "path";
-import Link from "next/link";
 import { GetStaticProps } from "next";
 import { parseFileByPath } from "@/lib/markdown";
 import { format, parse } from "date-fns";
 import Head from "next/head";
-import {
-  Box,
-  Container,
-  Hero,
-  HeroImageUtil,
-  Text,
-  LocalImage,
-  ShadowBox,
-  AspectRatio,
-  Button,
-  Flex,
-  VStack,
-} from "@/lib/ui";
-import Image from "next/image";
+import { Box, Container, Text, Flex } from "@/lib/ui";
 import {
   FilteredBlogsList,
   BlogItem,
 } from "@/components/FilteredBlogsList/FilteredBlogsList";
-import rainbow from "../../../assets/heroImages/blog/rainbow.svg";
-import rubiks from "../../../assets/heroImages/blog/rubiks.svg";
-import plus from "../../../assets/heroImages/blog/plus.svg";
 import generateBlogFeeds from "../../../lib/feeds";
 import { MEDIA_ITEMS } from "@/content/media/media";
 import { SearchField } from "@/components/SearchField/SearchField";
+import { FeaturedBlog } from "@/components/Blog/FeaturedBlog/FeaturedBlog";
 
 type Props = {
   blogItems: BlogItem[];
 };
-
-const rainbowImage = rainbow as LocalImage;
-const rubiksImage = rubiks as LocalImage;
-const plusImage = plus as LocalImage;
 
 export default function Blog({ blogItems }: Props) {
   const latestBlog = blogItems.find((blog) => {
@@ -67,91 +47,11 @@ export default function Blog({ blogItems }: Props) {
         />
       </Head>
       <Box mb="150px">
-        <Box
-          bg="purple.500"
-          borderBottom="1.5px solid black"
-          py={{
-            base: 8,
-            md: 16,
-          }}
-          px={{
-            base: 6,
-            md: 12,
-          }}
-          mb={{
-            base: 8,
-            md: "150px",
-          }}
-        >
-          <Container w="100%" maxW="container.lg">
-            <Flex
-              direction={{
-                base: "column",
-                lg: "row",
-              }}
-              alignItems={{
-                base: "stretch",
-                lg: "center",
-              }}
-            >
-              <Box
-                mb={{
-                  base: 8,
-                  lg: 0,
-                }}
-                w={{
-                  base: "100%",
-                  lg: "50%",
-                }}
-              >
-                <ShadowBox
-                  shadowColor="transparent"
-                  overflow="hidden"
-                  borderWidth="2px"
-                  borderRadius="4px"
-                >
-                  <AspectRatio ratio={465 / 309}>
-                    {latestBlog.image && (
-                      <Image alt="" src={latestBlog.image} fill />
-                    )}
-                  </AspectRatio>
-                </ShadowBox>
-              </Box>
-              <Flex
-                direction="column"
-                alignItems="flex-start"
-                justifyContent="center"
-                px={{
-                  base: 0,
-                  sm: 8,
-                  lg: 16,
-                }}
-                w={{
-                  base: "100%",
-                  lg: "50%",
-                }}
-              >
-                <Text textStyle="h4" textTransform="uppercase" mb={4}>
-                  Featured Blog
-                </Text>
-                <Text as="h3" textStyle="h3" marginBottom={4} minHeight="2.5em">
-                  {latestBlog.title}
-                </Text>
-                <Button
-                  as={Link}
-                  href={latestBlog.href}
-                  size="sm"
-                  bg="white"
-                  _hover={{
-                    bg: "gray.200",
-                  }}
-                >
-                  Read Now
-                </Button>
-              </Flex>
-            </Flex>
-          </Container>
-        </Box>
+        <FeaturedBlog
+          image={latestBlog.image}
+          title={latestBlog.title}
+          href={latestBlog.href}
+        />
         <Container
           w="100%"
           maxW="container.2xl"
