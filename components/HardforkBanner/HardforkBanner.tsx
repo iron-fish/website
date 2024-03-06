5a1a968989d5ebc9d2975bc6393ce868bc31cc86
Mat <mat@iflabs.network>
2024-03-06 10:51:16 -0700

    Add date logic: hide the banner after the date, auto-format the date string

--- components/HardforkBanner/HardforkBanner.tsx
+++ components/HardforkBanner/HardforkBanner.tsx
@@ -1,7 +1,14 @@
 import { Container, Flex, HStack, Link, Text } from "@/lib/ui";
 import { RightCaret } from "@/lib/ui/src/icons/RightCaret/RightCaret";
+import { format } from "date-fns";
+
+const HARDFORK_DATE = new Date("2024-04-02T12:00:00Z");
 
 export function HardforkBanner() {
+  if (new Date() > HARDFORK_DATE) {
+    return null;
+  }
+
   return (
     <Flex bg="black" h="52px" alignItems="center">
       <Container
@@ -12,7 +19,8 @@ export function HardforkBanner() {
       >
         <HStack>
           <Text color="white">
-            The hardfork is coming on April 2nd, 12 PM GMT.&nbsp;
+            The hardfork is coming on {format(HARDFORK_DATE, "MMMM do, h b z")}
+            .&nbsp;
           </Text>
           <Link href="/learn/blog/2024-02-26-testnet-hardfork">
             <HStack>
