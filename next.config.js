ae4b5ce0b4892f88ffa9357beaf54ab4aec18af9
Daniel Cortes <danielgcortes@gmail.com>
2024-03-13 11:46:55 -0600

    Fix newsletter sign up

--- next.config.js
+++ next.config.js
@@ -26,7 +26,7 @@ const nextConfig = {
           {
             key: "Content-Security-Policy",
             value: `
-script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://vercel.live;
+script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://vercel.live https://network.us20.list-manage.com;
 object-src 'none';
 base-uri 'self';
 form-action 'self';
