d4c5f654ad1ba5df3565ff7d8b07e5dc9394935d
Elena Nadolinski <elena@ironfish.network>
2024-06-13 17:51:44 -0700

    grants documentation page

---
+++ content/grants/sidebar.ts
@@ -0,0 +1,8 @@
+import { SidebarDefinition } from "@/lib/markdown";
+
+export const sidebar: SidebarDefinition = [
+  {
+    label: "Grants Overview",
+    items: ["how_to_apply", "active_grants", "grant_sizes"],
+  },
+];
---
+++ pages/community/grants/[...slug].tsx
@@ -0,0 +1,88 @@
+import { GetStaticPaths, GetStaticProps } from "next";
+import path from "path";
+import { MDXRenderer, SidebarItems } from "@/lib/ui";
+import {
+  getSidebarContent,
+  parseFileByPath,
+  renderMarkdown,
+  parseNestedDir,
+} from "@/lib/markdown";
+import { ComponentProps } from "react";
+import { DocumentationLayout } from "../../../layouts/Documentation/Documentation";
+import { sidebar } from "../../../content/grants/sidebar";
+
+const CONTENT_DIR = ["content", "grants"];
+
+type Props = {
+  slug: string;
+  frontMatter: {
+    title?: string;
+    description?: string;
+  };
+  markdown: ComponentProps<typeof MDXRenderer>["markdown"];
+  sidebarItems: SidebarItems;
+};
+
+export default function DocumentationPage({
+  frontMatter,
+  markdown,
+  sidebarItems,
+  slug,
+}: Props) {
+  return (
+    <DocumentationLayout
+      frontMatter={frontMatter}
+      markdown={markdown}
+      sidebarItems={sidebarItems}
+      githubPath={CONTENT_DIR.join("/") + `/${slug}.mdx`}
+      seoTitlePrefix="Iron Fish Grants Program | "
+    />
+  );
+}
+
+const CONTENT_PATH = path.join(process.cwd(), ...CONTENT_DIR);
+
+export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
+  console.log("IM HERE");
+  const slug = Array.isArray(params?.slug) ? params?.slug.join("/") : null;
+
+  if (typeof slug !== "string") {
+    throw new Error("Slug must be a string");
+  }
+
+  const { frontMatter, content } = parseFileByPath(
+    path.join(CONTENT_PATH, `${slug}.mdx`)
+  );
+
+  const markdown = await renderMarkdown(content);
+
+  const sidebarItems = getSidebarContent(sidebar, CONTENT_PATH, "/grants");
+
+  return {
+    props: {
+      slug: slug,
+      frontMatter,
+      markdown,
+      sidebarItems: sidebarItems,
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths<{
+  slug: Array<string>;
+}> = async () => {
+  const paths = parseNestedDir(CONTENT_PATH)
+    .filter((item) => {
+      return item[item.length - 1].endsWith(".mdx");
+    })
+    .map((item) => {
+      item[item.length - 1] = item[item.length - 1].replace(".mdx", "");
+      return {
+        params: {
+          slug: item,
+        },
+      };
+    });
+
+  return { paths, fallback: false };
+};
