4599d9e43d30068cc44a25b3a7aeb58dadfbe9e5
Daniel Cortes <danielgcortes@gmail.com>
2024-09-06 13:49:05 -0600

    Split up homepage safey section

--- components/HomePage/Safety/Safety.tsx
+++ components/HomePage/Safety/Safety.tsx
@@ -18,18 +18,6 @@ import oreoWalletImage from "./assets/oreowallet.png";
 import mobileAppImage from "./assets/mobile-app.png";
 import Image, { StaticImageData } from "next/image";
 
-const messages = defineMessages({
-  heading: {
-    id: "safety.heading",
-    defaultMessage: "Safety should be accessible",
-  },
-  description: {
-    id: "safety.description",
-    defaultMessage:
-      "We provide a user-friendly decentralized currency that's secure and convenient, while supporting sophisticated applications on our encrypted network.",
-  },
-});
-
 export function Safety() {
   const { formatMessage } = useIntl();
   return (
@@ -57,14 +45,16 @@ export function Safety() {
         }}
       >
         <Text as="h2" textStyle="h3" color="white" mb={8}>
-          {formatMessage(messages.heading)}
+          Safe, Accessible Wallets
         </Text>
-        <Text color="#CCC" textStyle="lg" mb={8}>
-          {formatMessage(messages.description)}
+        <Text color="#CCC" textStyle="lg" maxW="40ch" margin="0 auto 2rem">
+          We provide a user-friendly decentralized currency that&apos;s secure
+          and convenient, while supporting sophisticated applications on our
+          encrypted network.
         </Text>
 
-        <Grid templateColumns="1fr 1fr" gap={8}>
-          <GridItem>
+        <Grid templateColumns="1fr 1fr" gap={8} mb="128px">
+          <GridItem display="flex" alignItems="stretch">
             <ItemCard
               name="The Node App"
               description="Send, receive and bridge assets from your desktop while running your own node."
@@ -74,7 +64,27 @@ export function Safety() {
             />
           </GridItem>
 
-          <GridItem>
+          <GridItem display="flex" alignItems="stretch">
+            <ItemCard
+              name="OreoWallet"
+              description="Make secure transactions right from your browser."
+              href="/use/node-app"
+              linkText="Go to website"
+              imageSrc={oreoWalletImage}
+            />
+          </GridItem>
+        </Grid>
+
+        <Text as="h2" textStyle="h3" color="white" mb={8}>
+          Bi-Directional Bridging
+        </Text>
+        <Text color="#CCC" textStyle="lg" maxW="40ch" margin="0 auto 2rem">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </Text>
+
+        <Grid templateColumns="1fr 1fr" gap={8}>
+          <GridItem display="flex" alignItems="stretch">
             <ItemCard
               name="Chainport Bridge"
               description="Encrypt assets from 24+ chains and bridge them to your Iron Fish account."
@@ -84,22 +94,12 @@ export function Safety() {
             />
           </GridItem>
 
-          <GridItem>
+          <GridItem display="flex" alignItems="stretch">
             <ItemCard
-              name="OreoWallet"
-              description="Make secure transactions right from your browser."
-              href="/use/node-app"
-              linkText="Go to website"
-              imageSrc={oreoWalletImage}
-            />
-          </GridItem>
-
-          <GridItem>
-            <ItemCard
-              name="Mobile App"
-              description="Total accessibility with secure and encrypted transactions , no matter where you are."
-              href="/use/node-app"
-              linkText="COMING SOON"
+              name="Bridge.IronFish"
+              description="Bridge your $IRON directly to the Ethereum network."
+              href="https://testnet.bridge.ironfish.network/"
+              linkText="Go to Bridge"
               imageSrc={mobileAppImage}
             />
           </GridItem>
@@ -120,12 +120,12 @@ function FancyLink({
 }) {
   return (
     <Flex
-      display="inline-flex"
+      display="flex"
       alignItems="center"
       gap={2}
       as={Link}
       href={href}
-      pb={4}
+      pb={8}
       position="relative"
       color={color}
       _hover={{
@@ -172,6 +172,9 @@ function ItemCard({
       shadowColor="#242424"
       pt={16}
       px={8}
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
     >
       <Text as="h3" textStyle="h4" color="white" mb={8}>
         {name}
@@ -183,7 +186,7 @@ function ItemCard({
         {linkText}
       </FancyLink>
 
-      <HStack justifyContent="center">
+      <HStack justifyContent="center" mt="auto">
         <Image src={imageSrc} alt={name} />
       </HStack>
     </ShadowBox>
