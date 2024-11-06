50bc1a1ed99bab3c46fd7f3fc3853fcf0c72efae
Rahul Patni <rahul@iflabs.network>
2024-11-06 15:30:51 -0800

    hide ledger on the side bar

--- content/get-started/sidebar.ts
+++ content/get-started/sidebar.ts
@@ -3,6 +3,6 @@ import { SidebarDefinition } from "@/lib/markdown";
 export const sidebar: SidebarDefinition = [
   "get-started",
   "mining",
-  "ledger",
+  // "ledger", // TODO: Uncomment after Ledger Live launch
   "glossary",
 ];
