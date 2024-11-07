b3e88ca8cb9b2ff54daa1d826d092880f608b12b
Connor Johnston <cgjohn@umich.edu>
2024-11-07 10:12:20 -0800

    Updates to have coming soon

--- components/HomePage/Safety/Safety.tsx
+++ components/HomePage/Safety/Safety.tsx
@@ -137,7 +137,7 @@ export function Safety() {
               name={formatMessage(messages.nodeApp)}
               description={formatMessage(messages.nodeAppDescription)}
               href="/use/node-app"
-              linkText={formatMessage(messages.nodeAppLink)}
+              ctaText={formatMessage(messages.nodeAppLink)}
               imageSrc={nodeAppImage}
             />
           </GridItem>
@@ -147,7 +147,7 @@ export function Safety() {
               name={formatMessage(messages.oreoWallet)}
               description={formatMessage(messages.oreoWalletDescription)}
               href="https://oreowallet.com/"
-              linkText={formatMessage(messages.oreoWalletLink)}
+              ctaText={formatMessage(messages.oreoWalletLink)}
               imageSrc={oreoWalletImage}
             />
           </GridItem>
@@ -180,8 +180,8 @@ export function Safety() {
             <ItemCard
               name={formatMessage(messages.chainPortBridge)}
               description={formatMessage(messages.chainPortBridgeDescription)}
-              href="https://app.chainport.io/?from=ETHEREUM&to=IRONFISH"
-              linkText={formatMessage(messages.chainPortBridgeLink)}
+              // href="https://app.chainport.io/?from=ETHEREUM&to=IRONFISH"
+              ctaText="COMING SOON"
               imageSrc={chainportImage}
               imageContainerProps={{
                 paddingTop: {
@@ -200,8 +200,8 @@ export function Safety() {
             <ItemCard
               name="Bridge.IronFish"
               description={formatMessage(messages.ironFishBridgeDescription)}
-              href="https://bridge.ironfish.network/"
-              linkText={formatMessage(messages.ironFishBridgeLink)}
+              // href="https://bridge.ironfish.network/"
+              ctaText="COMING SOON"
               imageSrc={bridgeIronFishImage}
               imageContainerProps={{
                 padding: {
@@ -257,8 +257,8 @@ function FancyLink({
 type ItemCardProps = {
   name: string;
   description: string;
-  href: string;
-  linkText: string;
+  href?: string;
+  ctaText: string;
   imageSrc: StaticImageData;
   imageContainerProps?: ComponentProps<typeof HStack>;
 } & Omit<ShadowBoxProps, "children">;
@@ -267,17 +267,24 @@ function ItemCard({
   name,
   description,
   href,
-  linkText,
+  ctaText,
   imageSrc,
   imageContainerProps,
   ...rest
 }: ItemCardProps) {
+  const isLink = !!href;
+  const linkProps = isLink
+    ? {
+        as: Link,
+        href,
+        target: href.startsWith("http") ? "_blank" : undefined,
+        rel: href.startsWith("http") ? "noreferrer" : undefined,
+      }
+    : null;
+
   return (
     <ShadowBox
-      as={Link}
-      href={href}
-      target={href.startsWith("http") ? "_blank" : undefined}
-      rel={href.startsWith("http") ? "noreferrer" : undefined}
+      {...linkProps}
       bg="#242424"
       borderColor="#3B3B3B"
       shadowColor="#242424"
@@ -305,13 +312,21 @@ function ItemCard({
       }}
       {...rest}
     >
-      <Text as="h3" textStyle="h4" color="white" mb={8}>
+      <Text mb={8} as="h3" textStyle="h4" color="white">
         {name}
       </Text>
       <Text color="white" textStyle="md" mb={8}>
         {description}
       </Text>
-      <FancyLink color="pink.400">{linkText}</FancyLink>
+      {isLink ? (
+        <FancyLink color="pink.400">{ctaText}</FancyLink>
+      ) : (
+        <Box mb={8} bg="#2F3A1F" borderRadius="4px">
+          <Text px={6} py={2} color="#C7F182">
+            {ctaText}
+          </Text>
+        </Box>
+      )}
 
       <HStack flexGrow={1} alignItems="flex-end" {...imageContainerProps}>
         <Image src={imageSrc} alt={name} />
