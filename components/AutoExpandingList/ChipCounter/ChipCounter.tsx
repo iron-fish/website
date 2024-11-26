2b8a59627a2135f8da21b083ce56adbb2b749298
Rahul Patni <patnir@users.noreply.github.com>
2024-11-26 14:52:32 -0800

    Removes duplicate ChipCounter code (#760)
    Previously, this code was present in both ChipCounter and ExpandingItem components. This commit removes the duplicate code from ExpandingItem component and uses the ChipCounter component instead.

--- components/AutoExpandingList/ChipCounter/ChipCounter.tsx
+++ components/AutoExpandingList/ChipCounter/ChipCounter.tsx
@@ -1,27 +1,52 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, FlexProps, Text } from "@chakra-ui/react";
+import { useMemo } from "react";
 
 export function ChipCounter({
-  num,
   active,
+  spacer,
+  num = 0,
   color = "gray.200",
+  theme = "light",
+  ...rest
 }: {
-  num: number;
-  active: boolean;
-  color: string;
-}) {
+  active?: boolean;
+  spacer?: boolean;
+  num?: number;
+  color?: string;
+  theme: string;
+} & FlexProps) {
+  const { bg, color: textColor } = useMemo(() => {
+    if (active) {
+      return {
+        bg: color,
+        color: "black",
+      };
+    }
+
+    return {
+      bg: theme === "light" ? "black" : "white",
+      color: theme === "light" ? "white" : "black",
+    };
+  }, [active, color, theme]);
+
   return (
     <Flex
-      bg={active ? color : "black"}
+      bg={bg}
       borderRadius="full"
       h="30px"
+      minW="45px"
       w="45px"
       justifyContent="center"
       alignItems="center"
       transition="background-color 0.3s ease-in-out"
+      opacity={spacer ? 0 : 1}
+      {...rest}
     >
-      <Text textStyle="sm" color={active ? "black" : "white"}>
-        {num < 10 ? `0${num}` : num}.
-      </Text>
+      {!spacer && (
+        <Text textStyle="sm" color={textColor} transform="translateY(1px)">
+          {num < 10 ? `0${num}` : num}.
+        </Text>
+      )}
     </Flex>
   );
 }
