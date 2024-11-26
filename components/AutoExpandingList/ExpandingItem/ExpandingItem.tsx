12cf3b315938aa83dadc2ef4bc27ec028a61a29c
Rahul Patni <rahul@iflabs.network>
2024-11-26 14:45:13 -0800

    Removes duplicate ChipCounter code
    Previously, this code was present in both ChipCounter and ExpandingItem components. This commit removes the duplicate code from ExpandingItem component and uses the ChipCounter component instead.

--- components/AutoExpandingList/ExpandingItem/ExpandingItem.tsx
+++ components/AutoExpandingList/ExpandingItem/ExpandingItem.tsx
@@ -1,7 +1,8 @@
-import { Box, Flex, FlexProps, Text } from "@chakra-ui/react";
-import React, { ReactNode, useMemo, useRef } from "react";
+import { Box, Flex } from "@chakra-ui/react";
 import useResizeObserver from "@react-hook/resize-observer";
+import { ReactNode, useRef } from "react";
 import { IoChevronDown, IoChevronUp } from "react-icons/io5";
+import { ChipCounter } from "../ChipCounter/ChipCounter";
 
 type Theme = "light" | "dark";
 
@@ -107,55 +108,5 @@ export function ExpandingItem({
         </Box>
       </Box>
     </Box>
-  );
-}
-
-function ChipCounter({
-  active,
-  spacer,
-  num = 0,
-  color = "gray.200",
-  theme = "light",
-  ...rest
-}: {
-  active?: boolean;
-  spacer?: boolean;
-  num?: number;
-  color?: string;
-  theme: Theme;
-} & FlexProps) {
-  const { bg, color: textColor } = useMemo(() => {
-    if (active) {
-      return {
-        bg: color,
-        color: "black",
-      };
-    }
-
-    return {
-      bg: theme === "light" ? "black" : "white",
-      color: theme === "light" ? "white" : "black",
-    };
-  }, [active, color, theme]);
-
-  return (
-    <Flex
-      bg={bg}
-      borderRadius="full"
-      h="30px"
-      minW="45px"
-      w="45px"
-      justifyContent="center"
-      alignItems="center"
-      transition="background-color 0.3s ease-in-out"
-      opacity={spacer ? 0 : 1}
-      {...rest}
-    >
-      {!spacer && (
-        <Text textStyle="sm" color={textColor} transform="translateY(1px)">
-          {num < 10 ? `0${num}` : num}.
-        </Text>
-      )}
-    </Flex>
   );
 }
