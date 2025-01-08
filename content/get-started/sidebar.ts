bc03e0ab434f341bb2453912fcd4ee865c5b8622
Daniel Cortes <danielgcortes@gmail.com>
2025-01-08 13:06:02 -0700

    Remove get-started folder from content

--- content/get-started/sidebar.ts
+++
@@ -1,7 +0,0 @@
-import { SidebarDefinition } from "@/lib/markdown";
-
-export const sidebar: SidebarDefinition = [
-  "mining",
-  // "ledger", // TODO: Uncomment after Ledger Live launch
-  "glossary",
-];
