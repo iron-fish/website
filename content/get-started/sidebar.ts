9bed75513f403e95a98fe667e97d9b336e6c5afd
Daniel Cortes <danielgcortes@gmail.com>
2025-01-08 13:13:33 -0700

    Fix broken links to glossary

--- content/get-started/sidebar.ts
+++ content/get-started/sidebar.ts
@@ -3,5 +3,4 @@ import { SidebarDefinition } from "@/lib/markdown";
 export const sidebar: SidebarDefinition = [
   "mining",
   // "ledger", // TODO: Uncomment after Ledger Live launch
-  "glossary",
 ];
